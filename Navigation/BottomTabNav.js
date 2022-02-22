06adf6f2e156d83dd01d2365c0f2522ea22fea7b
masibasi <jimin_lee98@naver.com>
2022-02-23 01:07:51 +0900

    struct BottomTabNav, topContainer

--- Navigation/BottomTabNav.js
+++ Navigation/BottomTabNav.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {FitnessScreen} from '../Screens/FitnessScreen';
 
+const HomeIcon = require('../Img/naviIcHomeGrayText.png');
+const FitnessIcon = require('../Img/naviIcFitnessBkSelectText.png');
+const CommerceIcon = require('../Img/naviIcStore2GrayText.png');
+const MyIcon = require('../Img/naviIcMypageGrayText.png');
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNav = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={FitnessScreen} />
+    <Tab.Navigator
+      initialRouteName="피트니스"
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarInactiveTintColor: 'rgb(116,117,118)',
+        tabBarActiveTintColor: 'rgb(24,25,26)',
+        tabBarIcon: ({focused}) => {
+          if (route.name === '홈') {
+            return <Image source={HomeIcon} />;
+          } else if (route.name === '피트니스') {
+            return <Image source={FitnessIcon} />;
+          } else if (route.name === '커머스') {
+            return <Image source={CommerceIcon} />;
+          } else if (route.name === '마이') {
+            return <Image source={MyIcon} />;
+          }
+        },
+      })}>
+      <Tab.Screen name="홈" component={HomeScreen} />
+      <Tab.Screen name="피트니스" component={FitnessScreen} />
+      <Tab.Screen name="커머스" component={CommerceScreen} />
+      <Tab.Screen name="마이" component={MyScreen} />
     </Tab.Navigator>
   );
 };
@@ -21,10 +45,17 @@ const HomeScreen = () => {
     </View>
   );
 };
-const FitnessScreen = () => {
+const CommerceScreen = () => {
   return (
     <View>
-      <Text>Fitness</Text>
+      <Text>Commerce</Text>
+    </View>
+  );
+};
+const MyScreen = () => {
+  return (
+    <View>
+      <Text>My</Text>
     </View>
   );
 };
--- Screens/FitnessScreen.js
+++ Screens/FitnessScreen.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import styled from 'styled-components/native';
 
-const MainContainer = styled(View)`
-  background-color: red;
-`;
+import TitleImg from '../Img/mainContentstitle1Copy13.png';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default FitnessScreen = () => {
+const MainContainer = styled(View)`
+  background-color: pink;
+`;
+const TopContainer = styled(View)`
+  height: 56px;
+  width: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 23px;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+const TitleWrapper = styled(View)`
+  padding-bottom: 7px;
+`;
+const PageTitle = styled(Image)``;
+
+const IconWrapper = styled(View)`
+  flex-direction: row;
+  padding-bottom: 8px;
+`;
+const SearchButton = styled(TouchableOpacity)``;
+const SearchIcon = styled(Icon)`
+  margin-right: 8px;
+`;
+const NotificationButton = styled(TouchableOpacity)``;
+const NotificationIcon = styled(Icon)``;
+export const FitnessScreen = () => {
   return (
     <MainContainer>
-      <Text>HELLO</Text>
+      <TopContainer>
+        <TitleWrapper>
+          <PageTitle source={TitleImg} />
+        </TitleWrapper>
+        <IconWrapper>
+          <SearchButton>
+            <SearchIcon name="md-search-outline" size={24} color="rgb(0,0,0)" />
+          </SearchButton>
+          <NotificationButton>
+            <NotificationIcon
+              name="notifications-outline"
+              size={24}
+              color="rgb(0,0,0)"
+            />
+          </NotificationButton>
+        </IconWrapper>
+      </TopContainer>
     </MainContainer>
   );
 };
