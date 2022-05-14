a96f98d5664bcf389a2d06804500356cc60acdb2
masibasi <jimin_lee98@naver.com>
2022-05-15 04:54:40 +0900

    feat:(FE-68f7) PostDetailScreen 디자인 수정, (icon error 있음)

--- Screens/PostDetailScreen.js
+++ Screens/PostDetailScreen.js
@@ -1,17 +1,32 @@
 import React, {useState} from 'react';
-import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 import styled from 'styled-components';
 import {ThemeBox} from '../Components/ThemeBox';
 import {TopTab} from '../Components/TopTab_Post';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import LikeIcon from '../Icon/icHeartBk24.svg';
 import CommentIcon from '../Icon/icCommentBk24.svg';
-const Container = styled(ScrollView)`
-  background-color: green;
+
+const Container = styled(KeyboardAwareScrollView)`
+  background-color: white;
   flex: 1;
 `;
+const PostDetailWrapper = styled(View)`
+  justify-content: flex-end;
+  flex: 1;
+`;
+
 const MainContainer = styled(View)`
-  margin-top: 4px;
+  margin-top: 1px;
   border-color: rgb(246, 246, 248);
   border-top-width: 1px;
   background-color: white;
@@ -61,6 +76,7 @@ const ButtonContainer = styled(View)`
   padding-left: 16px;
   align-items: center;
   flex-direction: row;
+  background-color: white;
 `;
 const Likes = styled(Text)`
   font-size: 14px;
@@ -78,15 +94,46 @@ const Comments = styled(Text)`
 
 const CommentContainer = styled(View)`
   flex: 1;
-  background-color: yellow;
 `;
 
-const NoCommentDisplay = styled(View)`
-  flex: 1;
-  // background-color: white;
+const SmileyIconWrapper = styled(View)`
+  padding: 16px;
+  padding-top: 0px;
+`;
+const NoCommentText = styled(Text)`
+  color: rgb(132, 133, 137);
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const EnterCommentWrapper = styled(View)`
+  background-color: white;
+`;
+const EnterComment = styled(TextInput).attrs({
+  placeholder: '댓글을 입력해주세요.',
+})`
+  height: 40px;
+  background-color: rgb(246, 246, 248);
+  margin: 10px;
+  margin-left: 16px;
+  margin-right: 16px;
+  border-radius: 30px;
+  padding-left: 16px;
+  padding-right: 40px;
+`;
+const EnterCommentBtn = styled(TouchableOpacity)`
+  background-color: rgb(186, 187, 192);
+  border-radius: 30px;
+  width: 28px;
+  height: 28px;
+  position: absolute;
+  right: 22px;
+  top: 16px;
 `;
 
 import SmileyIcon from '../Icon/icSmile24.svg';
+import {NoCommentDisplay} from '../Components/NoCommentDisplay';
+
 export const PostDetailScreen = ({route, navigation}) => {
   const [likeClicked, setLikeClicked] = useState(true);
   const {
@@ -104,29 +151,33 @@ export const PostDetailScreen = ({route, navigation}) => {
   } = route.params.datas;
 
   return (
-    <Container>
+    <PostDetailWrapper>
       <TopTab navigation={navigation} />
-      <MainContainer>
-        <ThemeBox />
-        <PostTitle>{title}</PostTitle>
-        <DetailsWrapper>
-          <Nickname>{nickname}</Nickname>
-          <Dot>·</Dot>
-          <ElapsedTime>{elapsed_time}</ElapsedTime>
-        </DetailsWrapper>
-        <MainText>{mainText}</MainText>
-      </MainContainer>
-      <ButtonContainer>
-        <LikeIcon />
-        <Likes>{likes}</Likes>
-        <CommentIcon />
-        <Comments>{comments}</Comments>
-      </ButtonContainer>
-      <CommentContainer>
-        <NoCommentDisplay>
-          <SmileyIcon />
-        </NoCommentDisplay>
-      </CommentContainer>
-    </Container>
+      <Container>
+        <MainContainer>
+          <ThemeBox />
+          <PostTitle>{title}</PostTitle>
+          <DetailsWrapper>
+            <Nickname>{nickname}</Nickname>
+            <Dot>·</Dot>
+            <ElapsedTime>{elapsed_time}</ElapsedTime>
+          </DetailsWrapper>
+          <MainText>{mainText}</MainText>
+        </MainContainer>
+        <ButtonContainer>
+          <LikeIcon />
+          <Likes>{likes}</Likes>
+          <CommentIcon />
+          <Comments>{comments}</Comments>
+        </ButtonContainer>
+        <CommentContainer>
+          <NoCommentDisplay />
+        </CommentContainer>
+      </Container>
+      <EnterCommentWrapper>
+        <EnterComment />
+        <EnterCommentBtn />
+      </EnterCommentWrapper>
+    </PostDetailWrapper>
   );
 };
